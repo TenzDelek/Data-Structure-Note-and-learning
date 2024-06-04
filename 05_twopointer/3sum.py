e17eb87e6626c137275d9fba9111ef629583d8bb
TenzDelek <tibetdelek@gmail.com>
2024-06-04 22:09:37 +0530

    loop changes

--- 05_twopointer/3sum.py
+++ 05_twopointer/3sum.py
@@ -2,8 +2,8 @@
 class Node:
     def threesum(self,arr:list[int],n:int):
         res=set()
-        for i in range(0,n):
-            for j in range(i+1,n):
+        for i in range(0,n-2):
+            for j in range(i+1,n-1):
                 for k in range(j+1,n):
                     if(arr[i]+arr[j]+arr[k]==0):
                         print("i got in")
